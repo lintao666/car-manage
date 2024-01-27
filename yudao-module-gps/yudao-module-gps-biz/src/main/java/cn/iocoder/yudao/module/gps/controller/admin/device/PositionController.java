7f98b25370de9186b16bb569a1d9b57779c8e63b
lintao.chong <756447868@qq.com>
2024-01-27 22:06:20 +0800

    feat: 添加GPS-车辆定位-车辆数统计接口

--- yudao-module-gps/yudao-module-gps-biz/src/main/java/cn/iocoder/yudao/module/gps/controller/admin/device/PositionController.java
+++ yudao-module-gps/yudao-module-gps-biz/src/main/java/cn/iocoder/yudao/module/gps/controller/admin/device/PositionController.java
@@ -1,5 +1,6 @@
 package cn.iocoder.yudao.module.gps.controller.admin.device;
 
+import cn.iocoder.yudao.module.gps.controller.admin.device.vo.SummaryVO;
 import cn.iocoder.yudao.module.gps.dal.dataobject.position.VehiclePositionStatusDO;
 import cn.iocoder.yudao.module.gps.service.position.PositionService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
-@Tag(name = "管理后台 - GPS")
+@Tag(name = "管理后台 - GPS-车辆定位展示")
 @RestController
 @RequestMapping("gps/vehicle/position")
 @Validated
@@ -21,9 +22,32 @@ public class PositionController {
     @Resource
     private PositionService positionService;
 
+    /**
+     * 车辆定位展示
+     * 此页面有
+     */
     @GetMapping
-    @Operation(summary = "查询所有车辆定位信息")
-    public List<VehiclePositionStatusDO> vehiclePositionList() {
+    @Operation(summary = "查询所有车辆定位信息()")
+    public List<VehiclePositionStatusDO> vehiclePositionList(Long deptId, String carNumber, String vehicleMask) {
+
         return positionService.getList();
+    }
+
+    /**
+     * 统计当前时间的
+     * 车辆总数（有OBD设备ID绑定信息），
+     * 车辆在线数（ACC处于点火状态），
+     * 车辆停留数（ACC处于熄火状态并不处于离线状态），
+     * 车辆离线数（ACC处于熄火状态超过3天）
+     */
+    @GetMapping("summary")
+    @Operation(summary = "车辆数统计")
+    public SummaryVO vehicleSummary() {
+        SummaryVO summary = new SummaryVO();
+        summary.setTotal(2910);
+        summary.setOnline(1465);
+        summary.setOff(1377);
+        summary.setOffline(68);
+        return summary;
     }
 }
